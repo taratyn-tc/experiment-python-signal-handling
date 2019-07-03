59f40908ce01268f2defd50c4557ff4ff9953014
Tom Aratyn <taratyn@tucows.com>
2019-07-03 19:15:12 -0400

    Example signal handling in an event loop.

---
+++ async_io_signal_handling_by_loop_flag.py
@@ -0,0 +1,30 @@
+import asyncio
+import signal
+from datetime import datetime
+
+keep_going = True
+
+
+async def expensive_op(n: int = 0):
+    print(f"starting op at {datetime.utcnow()}")
+    await asyncio.sleep(n)
+    print(f"ending op at {datetime.utcnow()}")
+
+
+def signal_handler(loop):
+    print(f"got signal in loop {loop}")
+    global keep_going
+    keep_going = False
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGINT, lambda: signal_handler(loop))
+    while keep_going:
+        print("about to do expensive op")
+        await expensive_op(3)
+    print("exiting...")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
