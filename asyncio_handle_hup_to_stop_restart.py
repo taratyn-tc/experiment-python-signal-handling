e7f82d4dd2e060c9425db63019eb866066e76b30
Tom Aratyn <taratyn@tucows.com>
2019-07-04 15:18:15 -0400

    Signal handling for sighup and sigint with an asyncio loop.

--- asyncio_handle_hup_to_stop_restart.py
+++ asyncio_handle_hup_to_stop_restart.py
@@ -1,5 +1,6 @@
 import asyncio
 import signal
+import uuid
 
 from asyncio import AbstractEventLoop
 from datetime import datetime
@@ -9,6 +10,7 @@ class MyAIterator:
     def __init__(self):
         self.counter = 0
         self.keep_going = True
+        self.id = str(uuid.uuid4())
 
     def __aiter__(self):
         return self
@@ -20,7 +22,7 @@ class MyAIterator:
         self.counter += 1
         await asyncio.sleep(3)
         print(f"finished aiter op at {datetime.utcnow()}")
-        return self.counter
+        return f"from {self.id}: {self.counter}"
 
     def stop(self):
         self.keep_going = False
@@ -31,7 +33,8 @@ class HUPHandler:
         self.keep_going = True
         self.aiter = MyAIterator()
         loop = asyncio.get_running_loop()
-        loop.add_signal_handler(signal.SIGINT, lambda: self.signal_handler(loop))
+        loop.add_signal_handler(signal.SIGINT, lambda: self.sigint_handler(loop))
+        loop.add_signal_handler(signal.SIGHUP, lambda: self.sighup_handler(loop))
 
     async def expensive_op(self):
         print(f"starting op at {datetime.utcnow()}")
@@ -42,15 +45,50 @@ class HUPHandler:
         async for val in self.aiter:
             print(f"val is {val}")
 
-    def signal_handler(self, loop: AbstractEventLoop):
-        print("got signal")
+    async def restart_run(self, loop: AbstractEventLoop):
+        print("restarting run")
+        self.aiter = MyAIterator()
+        await self.run()
+
+    def sigint_handler(self, loop: AbstractEventLoop):
+        print("got sigint")
         self.aiter.stop()
+
+        # raising an exception doesn't work as it only bubbles up into the
+        # asyncio library code. I can't catch it in `main()` or in __main__.
+        # raise Exception("foobar")
+
+        # can't just call stop because that's too aggressive
+        # loop.stop()
+
+        def stop_if_only_one():
+            tasks = asyncio.all_tasks(loop)
+            if len(tasks) == 0:
+                print("work done, stopping loop")
+                loop.stop()
+            else:
+                print("left over work to do, check again in ~.5s.")
+                loop.call_later(0.5, stop_if_only_one)
+
+        loop.call_soon(stop_if_only_one)
+        print("exiting sigint")
+
+    def sighup_handler(self, loop: AbstractEventLoop):
+        print("got sighup")
+        self.aiter.stop()
+        loop.create_task(self.restart_run(loop))
+        print("done processing_sighup")
 
 
 async def main():
     handler = HUPHandler()
-    await handler.run()
+    loop = asyncio.get_running_loop()
+    loop.create_task(handler.run())
+    print("exiting main")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main_loop = asyncio.get_event_loop()
+    main_loop.create_task(main())
+    main_loop.run_forever()
+    print("end of script file.")
