d8a3fef508c642891e42581c09f6351fe6ca6a4a
Tom Aratyn <taratyn@tucows.com>
2019-07-15 18:47:13 -0400

    Restored fancy synchronous signal handling.

--- handle_signals.py
+++ handle_signals.py
@@ -1,24 +1,30 @@
-import datetime
+import signal
 import time
+from datetime import datetime
+
+keep_going = True
+
+
+def expensive_op(n: int = 0):
+    print(f"starting op at {datetime.utcnow()}")
+    time.sleep(n)
+    print(f"ending op at {datetime.utcnow()}")
+
+
+def signal_handler(signum, frame):
+    print(f"got signal {signum}")
+    global keep_going
+    keep_going = False
 
 
 def main():
-    try:
-        start_loop()
-    except KeyboardInterrupt as ki:
-        print(f"got {str(ki)} {repr(ki)}")
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    while keep_going:
+        print("about to do expensive op")
+        expensive_op(3)
+    print("exiting...")
 
 
-def start_loop():
-    while True:
-        time_consuming_op()
-
-
-def time_consuming_op():
-    print(f"started op {datetime.datetime.utcnow()}")
-    time.sleep(4)
-    print(f"ended op {datetime.datetime.utcnow()}")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
